4c8b1f7b8d501c8a9bf98562b04eec2b198e6fa1
James Kelly <jk377y@gmail.com>
2023-06-09 13:09:04 -0500

    updated lost component

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import glia from "../src/assets/images/glia.jpg";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+        <div className="lost">
+            <img src={glia} alt="glia" className="glia" />
+            <div className={`chat-bubble`}>
+                <div className="message">You have arrived in an empty parallel universe.</div>
+                <h2 style={{ textAlign: 'center' }}> ========== </h2>
+                <div className="message">Click one of the buttons at the top to return.</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
