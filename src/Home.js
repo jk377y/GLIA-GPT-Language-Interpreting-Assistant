e0b76b22ae873cd410d040bab055b98f55312623
James Kelly <jk377y@gmail.com>
2023-06-11 10:35:29 -0500

    rotated home messages to avoid delay on initial message

--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
 
 	useEffect(() => {
 		const messages = [
-			'Hello, I am <span class="highlight">GLIA</span>.',
 			'I am your \n<span class="highlight">G</span>PT \n<span class="highlight">L</span>anguage \n<span class="highlight">I</span>nterpreting \n<span class="highlight">A</span>ssistant.',
 			"I am glad that I can be of service today.",
 			'Feel free to visit the <a href="/chatbox" class="chatboxLink">Chatbox</a> and ask me any questions.',
 			'All of my responses are routed through the <a href="https://platform.openai.com/docs/api-reference" target="_blank" rel="noreferrer" class="highlight">OPENAI API</a>.',
+			'Hello, I am <span class="highlight">GLIA</span>.',
 		];
 
 		let currentIndex = 0;
